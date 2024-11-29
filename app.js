793cb701f1ae4e5b1f4dd6fd81beba423f8ed508
Gursharan Singh <gs7217013@gmail.com>
2024-11-29 22:22:44 +0100

    Completed MongoDB CRUD project with new features

--- app.js
+++ app.js
@@ -28,10 +28,22 @@ app.post('/create', async (req, res) => {
     res.redirect('/read');
 });
 
+app.get('/edit/:userid', async (req, res) => {
+    let user = await userModel.findOne({_id: req.params.userid});
+    res.render('edit', {user});
+});
+
+app.post('/update/:userid', async (req, res) => {
+    let {name, email, image} = req.body;
+    let updatedUser = await userModel.findOneAndUpdate({_id: req.params.userid}, {name, email, image}, {new: true});
+    res.redirect('/read');
+})
+
 app.get('/delete/:id', async (req, res) => {
     let users = await userModel.findOneAndDelete({ _id: req.params.id });
     res.redirect('/read');
 });
+
 
 
 app.listen(3000);
